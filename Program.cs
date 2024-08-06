6e72a63371ccf69a27dae2bc10c280ba095478c3
Natarajan-rangasamy <natraj2431@gmail.com>
2024-08-06 19:45:57 +0530

    new one

--- Program.cs
+++ Program.cs
@@ -3,33 +3,61 @@
     private static void Main(string[] args)
     {
 
-        Console.WriteLine("Password Checker.....!");
-        Console.WriteLine("Enter the password.....");
-        string ep = Console.ReadLine();
-        while (ep.Length == 0) {
-            Console.WriteLine("Please enter some text null values not allowed.........");
-            ep = Console.ReadLine();
-        }
-        Console.WriteLine("Please confirm your password again!");
-        string cp= Console.ReadLine();
-        while (cp.Length == 0) {
-            Console.WriteLine("Please enter some text null values not allowed.........");
-            cp = Console.ReadLine();
-        }
-        bool check = true;
-        while (check)
-        {
-            if (ep.Equals(cp))
-            {
-                Console.WriteLine("All done Your password has been set");
-                check = false;
-            }
-            else
-            {
-                Console.WriteLine("Password Mismatch!\nEnter the password again");
-                cp = Console.ReadLine();
-            }
-        }
+        /*  Console.WriteLine("Password Checker.....!");
+          Console.WriteLine("Enter the password.....");
+          string ep = Console.ReadLine();
+          while (ep.Length == 0) {
+              Console.WriteLine("Please enter some text null values not allowed.........");
+              ep = Console.ReadLine();
+          }
+          Console.WriteLine("Please confirm your password again!");
+          string cp= Console.ReadLine();
+          while (cp.Length == 0) {
+              Console.WriteLine("Please enter some text null values not allowed.........");
+              cp = Console.ReadLine();
+          }
+          bool check = true;
+          while (check)
+          {
+              if (ep.Equals(cp))
+              {
+                  Console.WriteLine("All done Your password has been set");
+                  check = false;
+              }
+              else
+              {
+                  Console.WriteLine("Password Mismatch!\nEnter the password again");
+                  cp = Console.ReadLine();
+              }
+          }*/
+        //odd and even list
+        /* List<int> odd = new List<int>();
+         List<int> even = new List<int>();
+         bool end = true;
+         Console.WriteLine("Press '0' key to End");
+         while (end) { 
+             int num = Convert.ToInt32(Console.ReadLine());
+             if(num == 0)
+             {
+                 end = false;
+             }
+
+             else if (num % 2 == 0) { 
+                 even.Add(num);
+             }
+             else
+             {
+                 odd.Add(num);
+             }
+
+         }
+         Console.WriteLine("Odd numbers");
+         foreach (int num in odd) {
+             Console.Write(num+" ");
+         }
+         Console.WriteLine("\nEven numbers");
+         foreach (int num in even) { Console.Write(num + " "); }*/
+        //Functions
 
     }
 }
