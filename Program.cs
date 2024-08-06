1b3767d405cbbc441807ccdf9283736e3fffb429
Natarajan-rangasamy <natraj2431@gmail.com>
2024-08-06 12:31:57 +0530

    Password Checker

--- Program.cs
+++ Program.cs
@@ -2,6 +2,34 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Password Checker.....!");
+        Console.WriteLine("Enter the password.....");
+        string ep = Console.ReadLine();
+        while (ep.Length == 0) {
+            Console.WriteLine("Please enter some text null values not allowed.........");
+            ep = Console.ReadLine();
+        }
+        Console.WriteLine("Please confirm your password again!");
+        string cp= Console.ReadLine();
+        while (cp.Length == 0) {
+            Console.WriteLine("Please enter some text null values not allowed.........");
+            cp = Console.ReadLine();
+        }
+        bool check = true;
+        while (check)
+        {
+            if (ep.Equals(cp))
+            {
+                Console.WriteLine("All done Your password has been set");
+                check = false;
+            }
+            else
+            {
+                Console.WriteLine("Password Mismatch!\nEnter the password again");
+                cp = Console.ReadLine();
+            }
+        }
+
     }
 }
